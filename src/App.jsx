44a51291a173a7c94e063124052d3d6edb644459
Sam Nithish <nithishraj8055@gmail.com>
2026-05-06 22:48:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import "./App.css";
 // Components
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-import Services from "./components/Services/Services";
+
 import Featured from "./components/featured/featured";
 import About from "./components/About/About";
 import Packages from "./components/packages/Packages";
 import Testimonials from "./components/testimonials/Testimonials";
 import Instagram from "./components/instagram/Instagram";
-import Blog from "./components/blog/Blog";
+
 import Footer from "./components/footer/footer";
 import Location from "./components/location/Location";
 import FloatingChat from "./components/floating/floating";
