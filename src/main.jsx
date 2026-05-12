aea259d218238a4f6c0fe9967e4715a16e6a037d
nithi075 <nithishraj8055@gmail.com>
2026-05-12 13:21:04 +0530

    init

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import {
-  BrowserRouter,
+  HashRouter,
 } from "react-router-dom";
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
 
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 
 );
